2c7cf477fda6631557e147c1cd48ed7ee3e08714
ShinzDev <shinaayomibamgboye@gmail.com>
2024-06-15 19:03:41 +0100

    commerce added to app

--- app/page.js
+++ app/page.js
@@ -1,8 +1,10 @@
-import Image from "next/image";
+import { Commerce } from "@/components/component/commerce";
 
+// import Image from "next/image";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main>
+        <Commerce/>
         Ecommerce flight hold the line 
     </main>
   );
